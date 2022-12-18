b1dad35a47945f7d57efdee7b66b1c2bbdb4c379
rwmehta <ramehta@umass.edu>
2022-12-18 00:32:16 -0500

    banners and torches

--- Assets/Scripts/level1.cs
+++ Assets/Scripts/level1.cs
@@ -6,7 +6,9 @@ using System.Linq;
 using UnityEngine.SceneManagement;
 public class level1 : MonoBehaviour
 {
-    public GameObject wall_prefab;     
+    public GameObject wall_prefab;
+    public GameObject banner_prefab;
+    public GameObject wallTorch_prefab;     
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +21,13 @@ public class level1 : MonoBehaviour
             r_wall.transform.position = new Vector3(10.0f, 0.0f, 5f*i - 17.5f);
             r_wall.transform.Rotate(0f, 270f, 0f);
             r_wall.transform.localScale = new Vector3(1.25f, 1f, 1f);
+            wallItem(10.0f, 5f*i - 17.5f, 270f);
             GameObject l_wall = Instantiate(wall_prefab, new Vector3(0, 0, 0), Quaternion.identity);
             l_wall.transform.position = new Vector3(-10.0f, 0.0f, 5f*i - 17.5f);
             l_wall.transform.Rotate(0f, 90f, 0f);
             l_wall.transform.localScale = new Vector3(1.25f, 1f, 1f);
+            wallItem(-10.0f, 5f*i - 17.5f, 90);
+
         }
         for (int i=1; i < 5; i++) {
             GameObject end_wall = Instantiate(wall_prefab, new Vector3(0, 0, 0), Quaternion.identity);
@@ -37,6 +42,27 @@ public class level1 : MonoBehaviour
                 mid_wall.transform.position = new Vector3(-8f + 4f*j, 0.0f, i);
                 mid_wall.transform.Rotate(0f, 180f, 0f);
                 // end_wall.transform.localScale = new Vector3(1.25f, 1f, 1f);
+            }
+        }
+    }
+
+    void wallItem(float x, float z, float rotation){
+        if(x > 0){
+            x = x-0.15f; 
+        }
+        else{
+            x += 0.15f;
+        }
+        if(Random.value > 0.3){
+            if (Random.value < 0.5f){
+                GameObject wallTorch = Instantiate(wallTorch_prefab, new Vector3(0, 0, 0), Quaternion.identity);
+                wallTorch.transform.position = new Vector3(x, 3.0f, z - 1.5f + Random.value*4);
+                wallTorch.transform.Rotate(0f, rotation, 0f);
+            }
+            else{
+                GameObject banner = Instantiate(banner_prefab, new Vector3(0, 0, 0), Quaternion.identity);
+                banner.transform.position = new Vector3(x, 3.0f, z - 1.5f + Random.value*4);
+                banner.transform.Rotate(0f, rotation, 0f);
             }
         }
     }
